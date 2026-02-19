77c64e8b863f9adfd88f0ef8afcd99452f2a6425
hdyuen <hoyuen@ucsd.edu>
2026-02-18 16:08:17 -0800

    Update main.cpp

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -12,48 +12,6 @@
 #include "util/util.h"
 
 #include <Arduino.h>
-uint32_t AppBuff[APPBUFF_SIZE];
-unsigned long lastSendTime = 0;
-
-extern const uint32_t g_ADigitalPinMap[];
-
-void dumpPin(uint8_t pin) {
-  uint32_t p = g_ADigitalPinMap[pin];
-
-  Serial.print("Arduino pin ");
-  Serial.print(pin);
-  Serial.print(" -> ");
-
-  Serial.print("P");
-  Serial.print((p >> 5) & 0x01);   // port (0 or 1)
-  Serial.print(".");
-  Serial.println(p & 0x1F);        // bit number
-}
-
-void findArduinoPin(uint8_t port, uint8_t bit) {
-  void* targetPort = (port == 0) ? NRF_P0 : NRF_P1;
-  uint32_t targetMask = (1UL << bit);
-
-  for (uint8_t pin = 0; pin < NUM_DIGITAL_PINS; pin++) {
-    if (digitalPinToPort(pin) == targetPort &&
-        digitalPinToBitMask(pin) == targetMask) {
-
-      Serial.print("P");
-      Serial.print(port);
-      Serial.print(".");
-      Serial.print(bit);
-      Serial.print(" -> Arduino pin ");
-      Serial.println(pin);
-      return;
-    }
-  }
-
-  Serial.print("P");
-  Serial.print(port);
-  Serial.print(".");
-  Serial.print(bit);
-  Serial.println(" -> not mapped to any Arduino pin");
-}
 
 
 //////////////////////////////////
@@ -81,13 +39,7 @@ void setup() {
   battery::init();
   bluetooth::init();
   sensor::init();
-//   Serial.printf(
-//   "Port: %u, BitMask: 0x%08lX\r\n",
-//   digitalPinToPort(PIN_MUX_A0_BIOZ),
-//   digitalPinToBitMask(PIN_MUX_A0_BIOZ)
-// );
-  // findArduinoPin(0, 24);  
-  // findArduinoPin(0, 25);  
+
 
 #ifdef DEBUG_MODE
   // dbgPrintDetailedPinStatus();
@@ -99,32 +51,5 @@ void setup() {
 }
 
 void loop() {
-  // #ifndef DEBUG_MODE
-  //   if (Serial.available()) {
-  //     String cmd = Serial.readStringUntil('\n');
-  //     cmd.trim();  // Remove any whitespace or newline
   
-  //     if (cmd == "reset") {
-  //       Serial.println("Resetting...");
-  //       delay(100);  // Ensure the message is sent before reset
-  //       NVIC_SystemReset();  // Software reset
-  //     }
-  //   }  
-    
-  //   //----- Send data over BLE if connected -----
-    
-  //   if (Bluefruit.connected() && isNewSampleReady()) {
-  //       uint16_t conn_handle = Bluefruit.Connection(0)->handle(); // first connected central
-  //       bluetooth::sendSensorData(conn_handle, AppBuff, APPBUFF_SIZE);
-  //   }
-  //   #endif
-    
-    // #ifdef DEBUG_MODE
-    // // // testing dummy data to flutter
-    // if (Bluefruit.connected() && millis() - lastSendTime > 1000) {
-    //   uint16_t conn_handle = Bluefruit.Connection(0)->handle(); // first connected central
-    //   bluetooth::sendSensorData(conn_handle, AppBuff, APPBUFF_SIZE);
-    //   lastSendTime = millis();
-    // } 
-    // #endif
 }
